6fe5467fa95f05ded6377fe214eb8a98df2a8e7c
星星 <73567024+CoreXing@users.noreply.github.com>
2022-11-09 18:04:26 +0800

    upload Lab06

--- LAB05/LAB05/script.js
+++ LAB05/LAB05/script.js
@@ -1,1 +1,20 @@
-//write your script here
+function init(){
+    document.getElementById("score").width = "100px";
+    document.getElementById("score").border = 1;
+    var tr = document.getElementsByTagName("td");
+    for(var i=0;i<tr.length;i++){
+        tr[i].bgColor = "white";
+    }
+}
+function chgTableW(number){
+    document.getElementById("score").width = number;
+}
+function chgBandSW(number){
+    document.getElementById("score").border = number;
+}
+function chgColor(color){
+    var tr = document.getElementsByTagName("td");
+    for(var i=3;i<tr.length;i++){
+        tr[i].bgColor = color;
+    }
+}
